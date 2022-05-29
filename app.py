d75fd32709426e996f61b3e489aeff6a5c0cfd6e
Oleks-Mel <80025493+Oleks-Mel@users.noreply.github.com>
2022-05-29 02:56:14 -0400

    New branch, buch of changes I'm too lazy to list, but it kinda works with flask now

--- app.py
+++ app.py
@@ -2,11 +2,21 @@
 #Run this and then click on the ip on the console
 
 #Importing flask to render the html and collect entered data
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__, static_url_path='/static')
+
 
 @app.route('/')
 def home():
    return render_template('index.html')
+
+@app.route('/about')
+def about():
+   return render_template('about.html')
+
+@app.route('/upload')
+def upload():
+   return render_template('upload.html')
+
 if __name__ == '__main__':
    app.run()
